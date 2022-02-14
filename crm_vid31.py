51d520dc92f7aab779f6027fbfa32069c1d716d9
Terry Dutcher <terryinclmich@hotmail.com>
2022-02-14 12:04:08 -0700

    Finished Video 32

--- crm_vid31.py
+++ crm_vid31.py
@@ -1,30 +1,29 @@
 from tkinter import *
-from PIL import ImageTk, Image
+from PIL import ImageTk,Image
 import mysql.connector
-import csv
 
 # Create a tkinter instance root is customary but can name it anything.
 root = Tk()
-
+    
 # Put a caption or title on our window
 root.title("Customer Records Management")
 root.iconphoto(False, PhotoImage(file='TD.png'))
-
+    
 # Set the size of the window
 root.geometry("400x600")
 
 # To get going, while in the venv.
 # sudo apt install mysql-server
-# gave it password password1234.
+# gave it password password1234. 
 # Using pip install mysql-connector gave an error so uninstalled it.
 # Then had to pip install mysql-connector-python.
 
 mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="password1234",
+    host = "localhost",
+    user = "root",
+    passwd = "password1234",
     # added after database was created
-    database="codemy",
+    database = "codemy", 
 )
 
 # This should tell us the connector worked.
@@ -69,8 +68,6 @@ my_cursor.execute("ALTER TABLE customers ADD ( \
 #    print(thing)
 
 # Clear Text Fields
-
-
 def clear_fields():
     first_name_box.delete(0, END)
     last_name_box.delete(0, END)
@@ -87,26 +84,15 @@ def clear_fields():
     price_paid_box.delete(0, END)
 
 # Submit Customer to Database
-
-
 def add_customer():
     sql_command = "INSERT INTO customers (first_name, last_name, zipcode, price_paid, email, address_1, address_2, city, state, country, phone, payment_method, discount_code) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    values = (first_name_box.get(), last_name_box.get(), zipcode_box.get(), price_paid_box.get(), email_box.get(), address1_box.get(
-    ), address2_box.get(), city_box.get(), state_box.get(), country_box.get(), phone_box.get(), payment_method_box.get(), discount_code_box.get())
+    values = (first_name_box.get(), last_name_box.get(), zipcode_box.get(), price_paid_box.get(), email_box.get(), address1_box.get(), address2_box.get(), city_box.get(), state_box.get(), country_box.get(), phone_box.get(), payment_method_box.get(), discount_code_box.get())
     my_cursor.execute(sql_command, values)
 
     # Commit the changes to the database
     mydb.commit()
     # when done adding, clear fields
     clear_fields()
-
-# Write to CSV file
-def write_to_csv(result):
-    # Going to append to the file.
-    with open('customers.csv', 'a', newline='') as f:
-        w = csv.writer(f, dialect='excel')
-        for record in result:
-            w.writerow(record)
 
 def list_customers():
     # First create a new window.
@@ -117,7 +103,6 @@ def list_customers():
     # Query database
     # Show the data in the database
     my_cursor.execute("SELECT * FROM customers")
-    # result will contain the whole database
     result = my_cursor.fetchall()
     # enumerated for loop creates an index number as well as x which is each row of the database
     for index, x in enumerate(result):
@@ -128,41 +113,25 @@ def list_customers():
             lookup_label = Label(list_customer_query, text=y)
             lookup_label.grid(row=index, column=num, sticky=W)
             num += 1
-    # button to export to csv file.
-    csv_button = Button(list_customer_query, text="Save to CSV",
-                        command=lambda: write_to_csv(result))
-    csv_button.grid(row=index+1, column=0)
 
 
 # Now create the gui for our application, first the labels
 title_label = Label(root, text="Customer Database", font=("Helvetica, 16"))
 title_label.grid(row=0, column=0, columnspan=2, pady="10")
 
-first_name_label = Label(root, text="First Name").grid(
-    row=1, column=0, sticky=W, padx=10)
-last_name_label = Label(root, text="Last Name").grid(
-    row=2, column=0, sticky=W, padx=10)
-address1_label = Label(root, text="Address 1").grid(
-    row=3, column=0, sticky=W, padx=10)
-address2_label = Label(root, text="Address 2").grid(
-    row=4, column=0, sticky=W, padx=10)
+first_name_label = Label(root, text="First Name").grid(row=1, column=0, sticky=W, padx=10)
+last_name_label = Label(root, text="Last Name").grid(row=2, column=0, sticky=W, padx=10)
+address1_label = Label(root, text="Address 1").grid(row=3, column=0, sticky=W, padx=10)
+address2_label = Label(root, text="Address 2").grid(row=4, column=0, sticky=W, padx=10)
 city_label = Label(root, text="City").grid(row=5, column=0, sticky=W, padx=10)
-state_label = Label(root, text="State").grid(
-    row=6, column=0, sticky=W, padx=10)
-zipcode_label = Label(root, text="Zipcode").grid(
-    row=7, column=0, sticky=W, padx=10)
-country_label = Label(root, text="Country").grid(
-    row=8, column=0, sticky=W, padx=10)
-phone_label = Label(root, text="Phone Number").grid(
-    row=9, column=0, sticky=W, padx=10)
-email_label = Label(root, text="Email Address").grid(
-    row=10, column=0, sticky=W, padx=10)
-payment_method_label = Label(root, text="Payment Method").grid(
-    row=11, column=0, sticky=W, padx=10)
-discount_code_label = Label(root, text="Discount Code").grid(
-    row=12, column=0, sticky=W, padx=10)
-price_paid_label = Label(root, text="Price Paid").grid(
-    row=13, column=0, sticky=W, padx=10)
+state_label = Label(root, text="State").grid(row=6, column=0, sticky=W, padx=10)
+zipcode_label = Label(root, text="Zipcode").grid(row=7, column=0, sticky=W, padx=10)
+country_label = Label(root, text="Country").grid(row=8, column=0, sticky=W, padx=10)
+phone_label = Label(root, text="Phone Number").grid(row=9, column=0, sticky=W, padx=10)
+email_label = Label(root, text="Email Address").grid(row=10, column=0, sticky=W, padx=10)
+payment_method_label = Label(root, text="Payment Method").grid(row=11, column=0, sticky=W, padx=10)
+discount_code_label = Label(root, text="Discount Code").grid(row=12, column=0, sticky=W, padx=10)
+price_paid_label = Label(root, text="Price Paid").grid(row=13, column=0, sticky=W, padx=10)
 
 # Now the entry boxes
 first_name_box = Entry(root)
@@ -193,15 +162,14 @@ price_paid_box = Entry(root)
 price_paid_box.grid(row=13, column=1, pady=5)
 
 # Buttons
-add_customer_button = Button(
-    root, text="Add Customer To Database", command=add_customer)
-add_customer_button.grid(row=14, column=0, padx=10, pady=10)
+add_customer_button = Button(root, text="Add Customer To Database", command=add_customer)
+add_customer_button.grid(row = 14, column=0, padx=10, pady=10)
 clear_fields_button = Button(root, text="Clear Fields", command=clear_fields)
 clear_fields_button.grid(row=14, column=1)
-list_customers_button = Button(
-    root, text="List Customers", command=list_customers)
+list_customers_button = Button(root, text="List Customers", command=list_customers)
 list_customers_button.grid(row=15, column=0, sticky=W, padx=10)
 
 
 # And the main event loop
 root.mainloop()
+
